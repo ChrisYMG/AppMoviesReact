25a0930d7d7b62beba6c64ecdf19b63e0090c210
ChrisYMG <cmejiagalindo@yahoo.com>
2022-04-22 10:59:01 -0500

    remove console.log_Hola

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import styles from "./App.module.css";
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import { MovieDetails } from "./pages/MovieDetails";
 import { LandingPage } from "./pages/LandingPage";
-console.log("Hola")
 export const App = () => {
   return (
     <Router>
